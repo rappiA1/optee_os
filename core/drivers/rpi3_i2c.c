1b656257cc89f2a3f364404512ce138a0e5c51b4
Raphael Andree <randree@leuze.com>
2021-10-28 16:48:45 +0200

    created i2c driver and header file for bcm2835 aka rpi3

---
+++ core/drivers/rpi3_i2c.c
@@ -0,0 +1,261 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * 
+ *
+ * I2C driver for BCM 2835 and similar
+ *
+ */
+#include <assert.h>
+#include <drivers/rpi3_i2c.h>
+#include <io.h>
+#include <kernel/boot.h>
+#include <kernel/dt.h>
+#include <kernel/delay.h>
+#include <libfdt.h>
+#include <mm/core_memprot.h>
+#include <string.h>
+
+/*
+ * Reset all register values in status and control register and disable i2c controller.
+ */
+void i2c_reset(vaddr_t base)
+{
+	struct i2c_regs *regs = (struct i2c_regs *)base;
+
+	/* disable BSC module */
+	io_clrbits32((vaddr_t)&regs->i2c_c, I2C_C_I2CEN);
+	
+	/* clear all interrupt configuration in control register. */
+	io_clrbits32((vaddr_t)&regs->i2c_c, I2C_C_INTR | I2C_C_INTT | I2C_C_INTD);
+
+	/* reset potentially set bits in status register */
+	io_setbits32((vaddr_t)&regs->i2c_s, I2C_S_CLKT | I2C_S_ERR | I2C_S_DONE);                                                
+}
+
+/*
+ * set i2c device base address, reset status and control registers
+ */
+TEE_Result i2c_init(struct bcm2835_i2c_data *i2c_data)
+{
+	/* 
+	 * searching BSC base address in device tree omitted, use hardcoded
+	 * BSC0 base address for now.
+	 * does dt_map_dev has to be called?, normally mapping is done 
+	 * automatically.
+	 */
+	vaddr_t ctrl_base = BSC0_BASE;
+
+	/* set base address in i2c_data structure */
+	i2c_data->base = ctrl_base;
+
+	/* clock divisor register is by default set to 0 which results in a 100 kHz I2C clock frequency */
+
+	/* reset control and status registers */
+	i2c_reset(ctrl_base);
+
+	return TEE_SUCCESS;
+}
+	
+/*
+ * Generate Stop Signal and disable I2C controller.
+ * regs		pointer to I2c controller registers
+ */
+static TEE_Result i2c_stop(struct i2c_regs *regs)
+{
+	/* disable I2C controller */
+	io_clrbits32((vaddr_t)&regs->i2c_c, I2C_C_I2CEN);
+
+	return TEE_SUCCESS;
+}
+
+
+static void i2c_fill_txfifo(struct i2c_regs *regs,
+		struct i2c_operation *i2c_operation)
+{
+	uint32_t val;
+
+	while (i2c_operation->length_in_bytes) {
+		val = io_read32((vaddr_t)&regs->i2c_s;
+
+		/* check if FIFO can accept data. */
+		if(!(val & I2C_S_TXD))
+			break;
+
+		/* write 8 bit value from buffer into fifo. */
+		io_write32((vaddr_t)&regs->i2c_fifo, *i2c_operation->buffer;
+
+		/* increment pointer buffer by one byte, decrease the number of bytes to be read */
+		i2c_operation->buffer++;
+		i2c_operation->length_in_bytes--;
+	}
+}
+
+/* 
+ * reads all data currently in the FIFO into the buffer of the i2c_operation object.
+ * i2c_regs		struct that maps the register addresses.
+ * i2c_operation	the operation object that contains operation info and buffer.
+ */
+static void i2c_drain_rxfifo(struct i2c_regs *regs,
+		struct i2c_operation *i2c_operation)
+{
+	uint32_t val;
+
+	/* while not all data is read */
+	while (i2c_operation->lentgth_in_bytes){
+
+		val = io_read32((vaddr_t)&regs->i2c_s;
+
+		/* check if FIFO still contains data */
+		if (!(val & I2C_S_RXD))
+			break;
+			
+		/* Write 32-bit value into array of 8 bit values, this works because the
+		 * BCM 2835 uses little endian and the LSB (lower 8 bits) which contains the 
+		 * data is read first, the rest of the DATA register are zeroes.
+		 */
+		*i2c_operation->buffer = io_read32(regs->i2c_fifo);
+
+		/* increment buffer pointer by one byte, decrease the number of bytes to be read. */
+		i2c_operation->buffer++;
+		i2c_operation->length_in_bytes--;
+	}
+}
+
+static TEE_Result i2c_write(uint32_t c_reg_flags, struct i2c_regs *regs,
+		struct i2c_operation *i2c_operation,
+		unsigned int slave_address)
+{
+	/* write slave address into address register. */
+	io_write32((vaddr_t)&regs->i2c_a, slave_address);
+
+	/* set data length register */
+	io_write32((vaddr_t)&regs->i2c_dlen, i2c_operation->length_in_bytes);
+
+	/* clear READ  in control register*/
+	io_clrbits32((vaddr_t)&regs->i2c_c, I2C_C_READ);
+
+	/* activate BSC, start transfer */
+	io_setbits32((vaddr_t)&regs->i2c_c, c);
+
+	/* do polling on TXW and DONE in status register to check if you
+	 * need to write to the fifo.
+	 */
+	while (1){
+		s_reg = io_read32((vaddr_t)&regs->i2c_s);
+
+		/* if fifo needs writing */
+		if (s_reg & I2C_S_TXW){
+			// can do error checking for the case that TXW is set but there is nothing to be written.
+			i2c_fill_txfifo(regs, i2c_operation);
+		}
+		else if (s_reg & I2C_S_DONE){
+			/* reset done flag */
+			io_clrbit32((vaddr_t)&regs->i2c_s, I2C_S_DONE);
+			return TEE_SUCCESS;
+		}
+	}
+}
+
+/*
+ * Read from the I2C bus into the buffer of the operation struct
+ * until all data is read from the I2C slave.
+ *
+ * c_reg_flags		Control register flags for activating BSC and read mode
+ * regs			Struct that points to the BSC registers.
+ * i2c_operation	Operation struct that contains the buffer to be written to
+ * slave_address	Address of the I2C slave
+ */
+static TEE_Result i2c_read(uint32_t c_reg_flags, struct i2c_regs *regs,
+		struct i2c_operation *i2c_operation,
+		unsigned int slave_address)
+{
+	C_reg_flags |= I2C_C_READ;
+	
+	/*
+	 * write slave address into address register, only 7 least significant
+	 * bit are relevant in A-Register.
+	 */
+	io_write32((vaddr_t)&regs->i2c_a, slave_address);
+	
+	/* set data length register */
+	io_write32((vaddr_t)&regs->i2c_dlen, i2c_operation->length_in_bytes);
+
+	/* activate BSC, start transfer and set read mode. */
+	io_setbits32((vaddr_t)&regs->i2c_c, c_reg_flags);
+
+	/* 
+	 * do polling on status register to check if fifo must be cleared or
+	 * transaction is done
+	 */
+	while (1){
+		s_reg = io_read32((vaddr_t)&regs->i2c_s);
+		if (s_reg & I2c_S_RXR){
+			i2c_drain_rxfifo(regs, i2c_operation);
+		}
+		else if (s_reg & I2C_S_DONE){
+			i2c_drin_rxfifo(regs, i2c_operation);
+
+			/* reset done flag */
+			io_clrbit32((vaddr_t)&regs->i2c_s, I2C_S_DONE);
+			return TEE_SUCCESS;
+		}
+	}
+
+}
+
+/*
+ * Sets the needed bits and starts a transfer.
+ * regs			pointer to I2C controller registers
+ * i2c_operation	pointer to I2C operation struct
+ * slave_address	address of I2C slave.
+ */
+static TEE_Result i2c_start_transfer(struct i2c_regs *regs,
+		struct i2c_operation *i2c_operation
+		unsigned int slave_address)
+{
+	uint32_t c = I2C_C_ST | I2C_C_I2EN;
+
+	if (i2c_operation->flags & I2C_FLAG_READ)
+		return i2c_read(c, regs, i2c_operation, slave_address);
+	else 
+		return i2c_write(c, regs, i2c_operation, slave_address);
+}
+
+/*
+ * transfer data to/from the I2C slave device.
+ */
+TEE_Result i2c_bus_xfer(vaddr_t base, unsigned int slave_address,
+			struct i2c_operation *i2c_operation,
+			unsigned int operation_count)
+{
+	unsigned int n = 0;
+	struct i2c_regs *regs = (struct i2c_regs *)base;
+	struct i2c_operation operation = NULL;
+	TEE_Result res = TEE_ERROR_GENERIC;
+
+	/* check that there is no read operation before the last operation */
+	for (n = 0, operation = i2c_operation;
+			n < (operation_count - 1); n++, operation++){
+		if (operation->flags & I2C_FLAG_READ){
+			EMSG("Only one read message supported, has to be last.");
+			goto out;
+		}
+	}
+	
+	/* go through all operation structs in the array and do the transfers */
+	for (n = 0, operation = i2c_operation;
+			n < operation_count; n++, operation++){
+	
+		/* read/write data */
+		res = i2c_start_transfer(regs, operation, slave_address);
+		if (res)
+			goto out;
+	}
+
+out:
+	i2c_stop(regs);
+
+	return res;
+	}
+
+}
---
+++ core/include/drivers/rpi3_i2c.h
@@ -0,0 +1,167 @@
+/*
+ * I2C driver for the BSC of the BCM2835.
+ * Author: Raphael Andree
+ */
+
+#ifndef __DRIVERS_BCM_I2C_H
+#define __DRIVERS_BCM_I2C_H
+
+#include <io.h>
+#include <stdint.h>
+#include <tee_api_types.h>
+
+/*
+ * base address of the BSC0 controller on BCM2835
+ */
+#define BSC0_BASE U(0xf220500)
+
+/* 
+ * I2C Enable
+ * 0 - BSC controller disabled.
+ * 1 - BSC controller enabled.
+ */
+#define I2C_C_I2CEN U(0x00008000)
+
+/*
+ * Interrupt on RX
+ * 0 - Don't generate interrupts on RXR
+ * 1 - Generate interrupt while rxr= 1
+ */
+#define I2C_C_INTR U(0x00000400)
+
+/*
+ * Interrupt on TX
+ * 0 - Don't generate interrupts on TXW condition
+ * 1 - Generate interrupt while TXW = 1
+ */
+#define I2C_C_INTT U(0x00000200)
+
+/*
+ * Interrupt on DONE
+ * 0 - Don't generate interrupts on DONE
+ * 1 - Generate interrupts while DONE = 1
+ */
+#define I2C_C_INTD U(0x00000080)
+
+/*
+ * ST Start Transfer
+ * 0 - No action
+ * 1 - Start a new transfer
+ */
+#define I2C_C_ST U(0x00000080)
+
+/*
+ * READ read transfer
+ * 0 - Write Packet transfer
+ * 1 - Read Packet transfer
+ */
+#define I2C_C_READ U(0x00000001)
+
+/* BSC status register bits, more info in bcm2835 peripherals documentation */
+#define I2C_S_CLKT U(0x00000200)
+#define I2C_S_ERR  U(0x00000100)
+#define I2C_S_RXD U(0x00000020)
+#define I2C_S_TXD U(0x00000010)
+#define I2C_S_RXR U(0x00000008)
+#define I2c_S_TXW U(0x00000004)
+#define I2C_S_DONE U(0x00000002)
+
+/* defines for flag in operation struct. */
+#define I2C_FLAG_WRITE U(0x00000000)
+#define I2C_FLAG_READ U(0x00000001)
+
+/*
+ * contains the register mapping, if an i2c_regs struct points to the 
+ * base address of the i2c MMIO.
+ */
+struct i2c_regs {
+        uint32_t i2c_c;		/* I2C Control register */
+        uint32_t i2c_s;		/* I2C Status register */
+        uint32_t i2c_dlen;	/* I2C Data length register */
+        uint32_t i2c_a; 	/* I2C Slave address register */
+        uint32_t i2c_fifo; 	/* I2C Data FIFO register */
+        uint32_t i2c_div;	/* I2C clock divider register */
+        uint32_t i2c_del;	/* I2C data delay register */
+        uint32_t i2c_clkt;	/* I2C clock stretch timeout register */
+};
+
+
+/*
+ * structure represents an I2C controller.
+ */
+struct bcm2835_i2c_data {
+	/* number of the I2C Controller to initialize */
+	uint8_t i2c_controller;
+	
+	/*
+	 * base will be filled by i2c_init() which will be used in
+	 * subsequent calls for reading/writing data.
+	 */
+	vaddr_t base;
+	
+	/* Raspberry pi core clock is nominally 150 MHz */
+	uint64_t i2c_core_clk;
+
+	/* Desired I2C speed to be able to calculate clock divider*/
+	uint64_t speed;
+};
+
+
+struct i2c_operation {
+	/* Flags to quailfy the I2c operation, e.g. read/write (see macros). */
+	uint32_t flags;
+
+	/*
+	 * Number of bytes to send or receive from the I2C device. A ping
+	 * is indicated by setting the length_in_bytes to zero
+	 */
+	unsigned int length_in_bytes;
+
+	/*
+	 * pointer to an array that can hold 8 bit data elements,
+	 * containing the data to send or to receive from the I2C device.
+	 * The buffer must be at least length_in_bytes size. The array
+	 * element size is 8 bit because 
+	 * 
+	 * Buffer gets allocated upon creation of the i2c_operation object.
+	 */
+	uint8_t *buffer;
+};
+
+/*
+ * Structure to fill for I2C read/write operation
+ * Has to be defined in the client application.
+ */
+struct i2c_reg_request {
+	 /* Number of operations to perform */
+	unsigned int operation_count;
+	/* Operation/-s to perform */
+	struct i2c_operation *operation;
+};
+	
+/*
+ * Initialize I2C controller, based on data passed
+ * in i2c_data.
+ */
+TEE_Result i2c_init(struct bcm2835_i2c_data *i2c_data);
+
+/*
+ * Software reset of the entire I2C module.
+ * The module is reset and disabled.
+ * Status register fields I2C_S are cleared.
+ * base		Base address of the I2C controllers registers
+ */
+void i2c_reset(vaddr_t base);
+
+/*
+ * Transfer data to/from I2c slave device
+ * base			Base Address of I2C controllers registers
+ * slave_address	Slave Address from which data is to be read
+ * i2c_operation	Pointer to an i2c_operation structure/array of them
+ * operation_count	Number of operations
+ */
+TEE_Result i2c_bus_xfer(vaddr_t base, uint32_t slave_address,
+		struct i2c_operation,
+		unsigned int operation_count);
+
+#endif /* __DRIVERS_BCM_I2C_H */
